936aef88904b4eccd83a00bc7f9b99c84022c923
kicheol <killguy00@naver.com>
2020-10-21 23:58:48 +0900

    SPM 설정 변경
    -- 누락된 라이브러리 추가

--- ImageSearchExample/Core/AppDelegate/SceneDelegate.swift
+++ ImageSearchExample/Core/AppDelegate/SceneDelegate.swift
@@ -5,12 +5,11 @@
 //  Created by 엄기철 on 2020/10/21.
 //
 
-
+import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
-
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
