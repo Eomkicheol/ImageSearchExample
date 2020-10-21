626143a319247ef5b8f399d46233a20e0e65d8b6
kicheol <killguy00@naver.com>
2020-10-21 23:47:48 +0900

    프로젝트 생성
    -- 폴더 물리 구조 생성
    -- SPM 라이브러리 추가

---
+++ ImageSearchExample/Core/AppDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ImageSearchExample
+//
+//  Created by 엄기철 on 2020/10/21.
+//
+
+import UIKit
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
